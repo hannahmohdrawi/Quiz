a5ede1773db62f50c398cc67d2bd56c945c582b1
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-07-04 22:40:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function recommend(){
+    
+
+
+    if (genreChoice == "pop" && feelingChoice == "blissful"){
+        songSelection.push(songsArray[1]);
+    } else if(genreChoice == "pop" && feelingChoice == "blissful"){
+
+    }
+
+    
+}
